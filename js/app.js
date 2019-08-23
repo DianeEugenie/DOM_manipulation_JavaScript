5fdeedfe0afa7ae60c8548be9c9e4bdcac8ba46a
Diane Jonker <Diane.eugenie.jonker@gmail.com>
2019-08-23 16:31:32 +0100

    Sets up HTML for lay out and form

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript has loaded');
 
   const form = document.querySelect('#new-djmon-form');
-  form.addEventListener('submit', handleForm);
+  form.addEventListener('submit', handleNewItemForm);
 
   // const emptyList = document.querySelector('#delete-all');
   // emptyList.addEventListener('click', handleEmptyList)
@@ -12,13 +12,32 @@ document.addEventListener('DOMContentLoaded', () => {
 const handleNewItemForm = function(event) {
   event.preventDefault();
 
-  const newDJmon = createNewDJmon(event.target);
+  const djmonListItem = createNewDJmon(event.target);
   const djmonList = document.querySelector('#djmon-list');
-  djmonList.appendChild(newDJmon);
+  djmonList.appendChild(djmonListItem;
 
   event.target.reset();
 };
 
 const createNewDJmon = function (form) {
-  
+  const djmonListItem = document.createElement('div');
+  djmonListItem.classList.add('djmon-list-item');
+
+  const name = document.createElement('h3');
+  name.tectContent = form.name.value;
+  djmonListItem.appendChild(name);
+
+  const djmon = document.createElement('h4');
+  name.tectContent = form.name.value;
+  djmonListItem.appendChild(name);
+
+  const instrument = document.createElement('h4');
+
+  const backSpin = document.createElement('h4');
+
+  const beatJuggle = document.createElement('h4');
+
+  const scratch = document.createElement('h4');
+
+  const slipCue = document.createElement('h4');
 }
