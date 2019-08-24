f7f36bff174ee0c185a710e8b9d2de28f923f0ad
Diane Jonker <Diane.eugenie.jonker@gmail.com>
2019-08-24 01:48:14 +0100

    Adds to checkbox so that text appears when ticking it

--- js/app.js
+++ js/app.js
@@ -69,10 +69,42 @@ const createNewDJmon = function (form) {
   venue.appendChild(venueValue);
   djmonItemBody.appendChild(venue)
 
+  const newCheckBox = document.createElement('input');
+  newCheckBox.type ='checkbox';
+
+  const newCheckBoxLabel = document.createElement('label');
+  newCheckBoxLabel.classList.add('not-seen-it');
+  const newCheckBoxText = document.createTextNode(`Been There!`);
+  newCheckBoxLabel.appendChild(newCheckBoxText);
+
+  newCheckBox.addEventListener('change', handleCheckBox);
+
+
+
   djmonListItem.appendChild(djmonItemBody);
+  djmonListItem.appendChild(newCheckBox);
+  djmonListItem.appendChild(newCheckBoxLabel);
+
+
 
   return djmonListItem;
 }
+
+const handleCheckBox = function(event) {
+  const seenIt = document.querySelector('.djmon-list-item-checked');
+  const notSeenIt = document.querySelector('.djmon-list-item');
+  const seenItText = document.querySelector('.seen-it');
+  const notSeenItText = document.querySelector('.not-seen-it');
+  
+  if (event.target.checked) {
+    event.target.parentElement.className = 'djmon-list-item-checked';
+    notSeenItText.className = 'seen-it'
+  } else {
+    event.target.parentElement.className = 'djmon-list-item';
+    seenItText.className = 'not-seen-it'
+  }
+
+};
 
 const handleEmptyList = function (event) {
   const readingList = document.querySelector('#djmon-list');
